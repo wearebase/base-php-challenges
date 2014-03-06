e25b7eea4fe2bf34b69c9992d38b8cb794789141
Dave Hulbert <dave1010@gmail.com>
2014-03-06 12:51:35 +0000

    Tweak rules as NeoThermic tried to cheat

--- challenges/the-mammoth.php
+++ challenges/the-mammoth.php
@@ -7,6 +7,7 @@ Rules::
 1. No use of Reflection API / runkit extension
 2. No stopping execution before Test::run()
 3. No Exceptions or PHP errors / warnings notices allowed
+4. No redefining $test
 
 Hints:
 1. Caesar
